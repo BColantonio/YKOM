8874f24bd7ea1f8b6faa964fa333a089440733e9
Brian <brian.j.colantonio@gmail.com>
2023-10-05 12:02:40 -0400

    tab navigation & home screen

--- routes/homestack.js
+++ routes/homestack.js
@@ -1,14 +1,38 @@
 import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import LoginScreen from '../screens/login';
+import HomeScreen from '../screens/home';
+
+// Import or replace SettingsScreen with the actual component
+// import SettingsScreen from '../screens/settings';
 
 const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+
+function MyTabs() {
+  return (
+    <Tab.Navigator
+      screenOptions={{ headerShown: false }}>
+      <Tab.Screen name="Home" component={HomeScreen} />
+      {/* Import or replace SettingsScreen */}
+      {/* <Tab.Screen name="Settings" component={SettingsScreen} /> */}
+    </Tab.Navigator>
+  );
+}
 
 export default function MyStack() {
   return (
     <Stack.Navigator>
-      <Stack.Screen 
-      options={{ headerShown: false }}
-      name="Login" component={LoginScreen} />
+      <Stack.Screen
+        options={{ headerShown: false }}
+        name="Login"
+        component={LoginScreen}
+      />
+      <Stack.Screen
+        options={{ headerShown: false }}
+        name="Home"
+        component={MyTabs}
+      />
     </Stack.Navigator>
   );
 }
--- screens/login.js
+++ screens/login.js
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
 import { globalStyles } from '../styles/global';
-import { SignUpScreen } from './signup';
 
 export default function LogInScreen({ navigation }) {
   return (
@@ -9,18 +8,17 @@ export default function LogInScreen({ navigation }) {
       <TextInput
         style={globalStyles.input}
         placeholder="Email"
-        onChangeText={this.handleEmail}
+        onChangeText={console.log('textchange')}
       />
       <TextInput
         style={globalStyles.input}
         placeholder="Password"
         secureTextEntry
-        onChangeText={this.handlePasswordChange}
+        onChangeText={console.log('textchange')}
       />
-      <Button title="log in" onPress={this.handleLogin} />
-      {/* "Sign Up" link */}
-      <Text style={globalStyles.subtext}>or</Text>
-      <Button title="sign up" onPress={this.handleSignup} />
+      <Button title="log in" onPress={() => navigation.navigate('Home')} />
+      {/* <Text style={globalStyles.subtext}>or</Text> */}
+      <Button title="sign up" onPress={console.log('handleSignup')} />
     </View>
   );
 }
