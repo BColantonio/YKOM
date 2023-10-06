b2f73b2ceea72dfe51eaccb16d927d93a6fdcc8d
Brian <brian.j.colantonio@gmail.com>
2023-10-06 11:01:33 -0400

    added example cards with dummy data to home screen

--- screens/home.js
+++ screens/home.js
@@ -1,11 +1,86 @@
 import * as React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Dimensions } from 'react-native';
 import { globalStyles } from '../styles/global';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import data from '../data';
+
+
+const maxVisibleItems = 8;
+
+const { width } = Dimensions.get('window')
+const _size = width * 0.9
+
+
+const layout = {
+  borderRadius: 16,
+  width: _size,
+  height: _size * 1.27,
+  spacing: 12,
+  cardsGap: 22,
+}
+
+function Card ({
+  info,
+  index,
+  totalLength
+}) {
+  return (
+    <View style={globalStyles.card}>
+      <Text
+        style={[
+          globalStyles.title,
+          {
+            position: 'absolute',
+            top: -layout.spacing,
+            right: layout.spacing,
+            fontSize: 102,
+            opacity: 0.05,
+          },
+        ]}
+        >
+          {index}
+      </Text>
+      <View style={globalStyles.cardContent}>
+        <Text style={globalStyles.title}>{info.type}</Text>
+        <View style={globalStyles.row}>
+          <Text style={globalStyles.subtitle}>
+            {info.from} - {info.to}
+          </Text>
+        </View>
+        <View style={globalStyles.row}>
+          <Text style={globalStyles.subtitle}>{info.distance} km</Text>
+        </View>
+        <View style={globalStyles.row}>
+          <Text style={globalStyles.subtitle}>{info.role}</Text>
+        </View>
+      </View>
+    </View>
+  )
+}
 
 export default function HomeScreen({ navigation }) {
   return (
-    <View style={globalStyles.container}>
-        <Text>Home</Text>
-    </View>
-  );
+    <GestureHandlerRootView style={globalStyles.container}>
+      <View
+        style={{
+          alignItems: 'center',
+          flex: 1,
+          justifyContent: 'flex-end',
+          marginBottom: layout.cardsGap * 2,
+        }}
+        pointerEvents="box-none"
+      >
+        {data.slice(0, 1).map((c, index) => {
+          return (
+            <Card
+              info={c}
+              key={c.id}
+              index={index}
+              totalLength={data.length - 1}
+            />
+          )
+        })}
+      </View>
+    </GestureHandlerRootView>
+  )
 }
--- styles/global.js
+++ styles/global.js
@@ -1,15 +1,51 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Dimensions } from 'react-native';
+const { width } = Dimensions.get('window')
+const _size = width * 0.9
+
+
+const layout = {
+  borderRadius: 16,
+  width: _size,
+  height: _size * 1.27,
+  spacing: 12,
+  cardsGap: 22,
+}
+
+const colors = {
+  primary: '#6667AB',
+  light: '#fff',
+  dark: '#111',
+}
 
 export const globalStyles = StyleSheet.create({
-    container: {
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center',
-    },
-    title: {
-      fontSize: 24,
-      marginBottom: 20,
-    },
+  container: {
+    flex: 1,
+    backgroundColor: colors.primary,
+    padding: layout.spacing,
+  },
+  card: {
+    borderRadius: layout.borderRadius,
+    width: layout.width,
+    height: layout.height,
+    padding: layout.spacing,
+    backgroundColor: colors.light,
+  },
+  title: { fontSize: 32, fontWeight: '600' },
+  subtitle: {},
+  cardContent: {
+    gap: layout.spacing,
+    marginBottom: layout.spacing,
+  },
+  locationImage: {
+    flex: 1,
+    borderRadius: layout.borderRadius - layout.spacing / 2,
+  },
+  row: {
+    flexDirection: 'row',
+    columnGap: layout.spacing / 2,
+    alignItems: 'center',
+  },
+  icon: {},
     subtext: {
       fontSize: 12,
       marginBottom: 0,
